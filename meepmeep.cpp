944a33c2f2d8d319c8c9c1624b50be5bfdd2ff38
Josh Galofaro <galo9664@fredonia.edu>
2011-10-19 15:00:12 -0400

    Finshed 2nd test puzzle.

---
+++ meepmeep.cpp
@@ -0,0 +1,24 @@
+/**
+ * meepmeep.cpp
+ *
+ * Author: Josh Galofaro
+ * Date: 10/19/2011
+ *
+ * Discription:
+ *  Meep meep!
+ *      To help test whether your puzzle submissions fit the guidelines, try 
+ *      this simple test puzzle. Your solution must follow the guidelines like 
+ *      any other puzzle. Write a program that takes as input a single argument 
+ *      on the command line. Normally this argument must be the name of a text 
+ *      file; however, for this puzzle your program should ignore the argument 
+ *      given. Instead, your program must print to standard out the string 
+ *      "Meep meep!" (without the double quotes and with exact capitalization) 
+ *      followed by a single newline (not zero or two). 
+ **/
+#include <iostream>
+
+int main( int argc, char *argv[] )
+{
+    std::cout << "Meep meep!" << std::endl;
+    return 1;
+}
